08c311d0fad90d35669b581287d02cabe9ef44fa
zhangshaoqiang <zhangshaoqiang@example.com>
2020-02-12 11:47:40 +0800

    使用mybatis的逆向工程生成pojo类还有mapper类，参考网址https://blog.csdn.net/qq_38455201/article/details/79194792

---
+++ mybatis-generator/src/main/java/com/car4s/generator/mapper/TbUserMapper.java
@@ -0,0 +1,30 @@
+package com.car4s.generator.mapper;
+
+import com.car4s.generator.pojo.TbUser;
+import com.car4s.generator.pojo.TbUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbUserMapper {
+    int countByExample(TbUserExample example);
+
+    int deleteByExample(TbUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbUser record);
+
+    int insertSelective(TbUser record);
+
+    List<TbUser> selectByExample(TbUserExample example);
+
+    TbUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByExample(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByPrimaryKeySelective(TbUser record);
+
+    int updateByPrimaryKey(TbUser record);
+}
