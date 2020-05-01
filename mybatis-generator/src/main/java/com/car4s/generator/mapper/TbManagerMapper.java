5bed644d8a6ec4968948ce75340ce3994afe70d7
zhangshaoqiang <zhangshaoqiang@example.com>
2020-05-01 09:23:15 +0800

    答辩完啦

---
+++ mybatis-generator/src/main/java/com/car4s/generator/mapper/TbManagerMapper.java
@@ -0,0 +1,30 @@
+package com.car4s.generator.mapper;
+
+import com.car4s.generator.pojo.TbManager;
+import com.car4s.generator.pojo.TbManagerExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbManagerMapper {
+    int countByExample(TbManagerExample example);
+
+    int deleteByExample(TbManagerExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbManager record);
+
+    int insertSelective(TbManager record);
+
+    List<TbManager> selectByExample(TbManagerExample example);
+
+    TbManager selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbManager record, @Param("example") TbManagerExample example);
+
+    int updateByExample(@Param("record") TbManager record, @Param("example") TbManagerExample example);
+
+    int updateByPrimaryKeySelective(TbManager record);
+
+    int updateByPrimaryKey(TbManager record);
+}
---
+++ mybatis-generator/src/main/java/com/car4s/generator/pojo/TbManager.java
@@ -0,0 +1,33 @@
+package com.car4s.generator.pojo;
+
+public class TbManager {
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
